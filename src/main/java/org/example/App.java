6e557c55da206c1ac3d784388271646f5bb4c5b6
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-10-12 22:42:14 +0700

    Приборочка. Перенес запросы и soat в метод Birthday

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,15 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
+        Birthday.myAge();
         
-        System.out.print("Мой возраст в днях: ");
+       /* System.out.print("Мой возраст в днях: ");
         System.out.println(Birthday.getAge(1980,7,1));
         System.out.println(" -------------------------------------------------- ");
         System.out.print("До следующего тысячного дня рождения:");
         System.out.print(1000 - ((Birthday.getAge(1980,7,1)) % 1000));
         System.out.println(" дней");
         System.out.print("Дата следующего тысячеденного дня рождения: ");
-        System.out.println(Birthday.nextBirthday(1980,7,1));
+        System.out.println(Birthday.nextBirthday(1980,7,1));*/
 
 
 
