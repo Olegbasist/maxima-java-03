633195b4fb69e4e962ab8a5ea7a05e982246ca1b
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-10-12 22:52:21 +0700

    Создал Cat
    Описал класс Cat, добавил переменные и геттеры/сеттеры

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,38 @@
+package org.example;
+
+// 29.09.2022 Описать класс Cat (кот) со свойствами:
+//  name — имя, строка.
+//  weight — вес, число int
+//  isAngry — сердитость, boolean
+//  Описать геттеры и сеттеры всех свойств.
+
+public class Cat {
+    
+    private String name;
+    private int weight;
+    private boolean isAngry;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isAngry() {
+        return isAngry;
+    }
+
+    public void setAngry(boolean angry) {
+        isAngry = angry;
+    }
+}
