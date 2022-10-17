0d14d14f1c3f8ce31efca78951d79aae11eb0e7b
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-10-17 18:14:53 +0700

    Потренировался читать и записывать

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,11 +12,16 @@ package org.example;
 // TODO: 29.09.2022 Описать класс, реализующий этот интерфейс:
 //  TextTransformer, при помощи классов FileReader / FileWriter
 
-public class App 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class App
 {
     public static void main( String[] args ) throws Exception {
 
-        System.out.println("-------------------------------");
+        /*System.out.println("-------------------------------");
 
 
         Cat kitty = new Cat("Пушок", 3,false);
@@ -29,9 +34,31 @@ public class App
 
         Cat cat = CatFactory.createCat("Рыжик", 51);
         System.out.print(!cat.isAngry() ? "Добрый " : "Злой ");
-        System.out.println(cat.getName() + " весом " + cat.getWeight() + " кило");
+        System.out.println(cat.getName() + " весом " + cat.getWeight() + " кило");*/
 
         System.out.println("-------------------------------");
+        System.out.println("-------------------------------");
+
+        Path myCsv = Paths.get("AllCatWeCanGet.csv");
+        boolean exist = Files.exists(myCsv);
+        System.out.println(exist ? ("Файл " +String.valueOf(myCsv)+ " существует") : "Файл не обраружен!");
+
+        FileWriter writer = new FileWriter(String.valueOf(myCsv), true);
+        String text = "Джордж;4;true";
+        writer.write("\n" +text);
+        writer.close();
+
+
+        FileReader reader = new FileReader(String.valueOf(myCsv));
+        int i =0;
+
+        while((i = reader.read())!=-1 ) {
+        System.out.print((char) i);
+        }
+        reader.close();
+
+
+
 
     }
 }
--- src/main/java/org/example/TextTransformer.java
+++ src/main/java/org/example/TextTransformer.java
@@ -1,9 +1,12 @@
 package org.example;
 
+// TODO: 29.09.2022 Описать класс, реализующий интерфейс Transformable :
+//  (TextTransformer), при помощи классов FileReader / FileWriter
+
 public class TextTransformer implements Transformable {
 
     @Override
     public void transform(String fileIn, String fileOut) {
-        
+
     }
 }
