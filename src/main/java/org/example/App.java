e0c9e736a58001c7f7a46cfccd2ee600d0211228
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-11-05 18:15:25 +0700

    Работает с латиницей (utf-8)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,6 +15,7 @@ package org.example;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Stream;
 
 public class App
@@ -23,24 +24,29 @@ public class App
 
 
         //TextTransformer transformer = new TextTransformer();
-        String fileIn = "myCats.csv";
-        String fileOut = "myCats.txt";
+        String fileIn = "myEngCats.csv";
+        String fileOut = "myEngCats.txt";
         //transformer.transform(fileIn,fileOut);
 
 // Тренируемся получать поток из файла
 
-        FileInputStream fileInputStream = new FileInputStream(fileIn);
-        Reader reader = new InputStreamReader(fileInputStream, Charset.forName("utf-8"));
+        FileInputStream fileInputStream = new FileInputStream(fileIn); ///
+
+
+
+        // Вывод в консоль
+        /*Reader reader = new InputStreamReader(fileInputStream, Charset.forName("utf-8"));
         BufferedReader bufferedReader = new BufferedReader(reader);
 
 
 
         Stream<String> stream = bufferedReader.lines();
         stream.forEachOrdered((s -> System.out.println(s)));
-        System.out.println(reader.toString().getBytes("utf-8"));
+        System.out.println(reader.toString().getBytes("utf-8"));*/
 
 // Тренируемся читать поток побайтово в цикл
-        /*System.out.println("-----------------------------------------------------");
+
+        System.out.println("-----------------------------------------------------");
 
         StringBuilder stringBuilder = new StringBuilder();
         int r;
@@ -50,9 +56,25 @@ public class App
 
         } while (r!=-1);
 
-        //System.out.println(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+        System.out.println(stringBuilder);
+//Записываем результат в файл
+        System.out.println("-----------------------------------------------------");
+        FileOutputStream fileOutputStream = new FileOutputStream(fileOut);
+        fileOutputStream.write(stringBuilder.toString().getBytes(StandardCharsets.UTF_8));
+        fileOutputStream.flush();
+        fileOutputStream.close();
 
-       FileInputStream stream2 = new  FileInputStream(fileIn);
+        FileInputStream fileInputStream2 = new FileInputStream(fileOut);
+        stringBuilder.delete(0,stringBuilder.capacity());
+        int s;
+        do{
+            s = fileInputStream2.read();
+            stringBuilder.append((char) s);
+
+        } while (s!=-1);
+
+        System.out.println(stringBuilder);
+      /* FileInputStream stream2 = new  FileInputStream(fileIn);
         StringBuilder result = new StringBuilder();
 
         do {
@@ -63,10 +85,13 @@ public class App
 
 // Тренируемся записывать в файл
 
-        FileOutputStream fileOutputStream = new FileOutputStream("test.txt");
+        /*OutputStream fileOutputStream = new FileOutputStream("test.txt");
 
-        //fileOutputStream.write(stream.toString().getBytes(Charset.forName("utf-8")));
-        fileOutputStream.close();
+        fileOutputStream.write(stream.toString().getBytes(Charset.forName("utf-8")));
+        fileOutputStream.write(reader.toString().getBytes(Charset.forName("utf-8")));
+
+        fileOutputStream.flush();
+        fileOutputStream.close();*/
 
 
     }
