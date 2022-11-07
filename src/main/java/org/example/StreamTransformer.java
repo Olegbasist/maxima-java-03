c79ad45c8397e2e3744d26d0dacd8fe385f2bbab
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-11-07 22:47:39 +0700

    Поправил. В консоль выводит стринг

--- src/main/java/org/example/StreamTransformer.java
+++ src/main/java/org/example/StreamTransformer.java
@@ -3,9 +3,9 @@ package org.example;
 //  29.09.2022 Описать класс StreamTransformer, реализующий интерфейс Transformable
 //   при помощи классов FileInputStream / FileOutputStream
 
-import sun.security.util.IOUtils;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class StreamTransformer implements Transformable {
 
@@ -23,14 +23,21 @@ public class StreamTransformer implements Transformable {
             }
 
 
-            String str = new String(byteArrayOutputStream.toByteArray(), "UTF-8");
+            String str = byteArrayOutputStream.toString("UTF-8"); // Еще один способ прочитать байты в правильной кодировке в строку
             System.out.println(str);
 
 
-            FileOutputStream outputStream = new FileOutputStream(fileOut);
+
+               /* writer.write(((Boolean.parseBoolean(cellData[2])) ? "Сердитый" : "Дружелюбный") + " кот " + cellData[0] + " весом " + cellData[1] + "кг.");
+                writer.newLine();
+                writer.flush();*/
+            }
+
+
+            /*FileOutputStream outputStream = new FileOutputStream(fileOut);
             outputStream.write((byteArrayOutputStream.toString().getBytes()));
             outputStream.flush();
-            outputStream.close();
+            outputStream.close();*/
 
         }catch (IOException e) {
             e.printStackTrace();
