a91221c1bf31ab61e9e03d41f523422a1cd787c2
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-11-17 16:51:09 +0700

    Добавил подсказку

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,8 @@ package org.example;
 //
 // TODO: 03.11.2022 Описать два класса QueueKitchen и StackKitchen,
 //  реализующие кормление животных по принципам FIFO и LIFO соответственно. (Подсказка: это конвейер)
-//  Подсказка: В каждом классе должно быть определено свойство animals типа ArrayList (подсказка Arraylist<>getAnimals)
+//  Подсказка: В каждом классе должно быть определено свойство animals типа ArrayList
+//      (подсказка Arraylist<>getAnimals) -> (уже делал в модуле 2 дз TransportToCityPlusLogistic)
 //  Подсказка из видео: В одну очередь ставить кошек и собак нельзя, но мы не знаем кого из них подадут на вход.
 
 public class App 
@@ -38,5 +39,46 @@ public class App
         System.out.println(doggy.getName() + " весом " + doggy.getWeight() + " кило");
 
         System.out.println("-------------------------------");
+
+
+    //Подсказка с ArrayList из транспортов ту сити:
+        // Добавляем все вихикл в ЭррейЛист и затем ищем минимум
+
+        /*ArrayList<Transport> arrOfTransports = new ArrayList<>();
+        //arrOfTransports.add(vehicles[0]);
+        //arrOfTransports.add(vehicles[1]);
+        Collections.addAll(arrOfTransports, vehicles);
+        arrOfTransports.forEach(elem -> System.out.println(elem));
+        for (Transport obj: arrOfTransports){
+            //Collections.addAll(objectsArrayList, obj.getPrice(city2));
+            float costOfDelivery = obj.getPrice(city2);
+            //System.out.println(obj);
+            System.out.print(obj.getName());
+            System.out.print(" доставит груз за - ");
+            System.out.println(costOfDelivery);
+            if (minCostOfDelivery > costOfDelivery && costOfDelivery !=0) {
+                minCostOfDelivery = costOfDelivery;
+                cheapestVehicle = obj;
+            };*/
+
+
+        // Посылаем объекты в Logistics заполняя ими vehicles
+
+
+
+       // Logistics logistics = new Logistics(vehicles);
+
+
+        /*logisticsWithoutList.setVehicles(vehicles[0]);
+        logisticsWithoutList.setVehicles(vehicles[1]);
+        logisticsWithoutList.setVehicles(vehicles[2]);
+        logisticsWithoutList.setVehicles(vehicles[3]);
+        logisticsWithoutList.setVehicles(vehicles[4]);*/
+        //logisticsWithoutList.setVehicles(new Truck("Грузищще", 5864, 120, 1.1f));
+//        System.out.println("Вот какие транспорты получились:");
+//        logistics.listOfVehicles();
+
     }
+
+
 }
