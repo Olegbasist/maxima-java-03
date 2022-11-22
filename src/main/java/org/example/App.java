b81e23071b1cc1fcdfe1ab62a56647a646939b05
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2022-11-22 16:01:11 +0700

    Создал класс CatStatistics

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,20 @@
 package org.example;
 
-// TODO: 07.10.2022 Описать класс CatStatistics со статическими методами:
-//      ArrayList<Cat> sortByNameAscending(ArrayList<Cat> cats) — возвращает список котов, отсортировав по имени по возрастанию.
-//      ArrayList<Cat> sortByWeightDescending(ArrayList<Cat> cats) — возвращает список котов, отсортировав по убыванию веса.
-//      ArrayList<Cat> removeFirstAndLast (ArrayList<Cat> cats) — возвращает список котов кроме первого и последнего.
-//      Cat findFirstNonAngryCat (ArrayList<Cat> cats) — возвращает первого не-сердитого кота в списке.
-//      int getCommonWeight(ArrayList<Cat> cats, boolean onlyAngry) — возвращает суммарный вес всех котов (если параметр onlyAngry равен true, то только сердитых котов).
-//      Map<String, List<Cat>> groupCatsByFirstLetter (ArrayList<Cat> cats) — возвращает список котов сгруппировав их по первой букве имени и отсортировав группировку по возрастанию.
+//  07.10.2022 Описать класс CatStatistics со статическими методами:
+//      TODO: 07.10.2022 ArrayList<Cat> sortByNameAscending(ArrayList<Cat> cats)
+//          — возвращает список котов, отсортировав по имени по возрастанию.
+//      TODO: 07.10.2022 ArrayList<Cat> sortByWeightDescending(ArrayList<Cat> cats)
+//          — возвращает список котов, отсортировав по убыванию веса.
+//      TODO: 07.10.2022 ArrayList<Cat> removeFirstAndLast (ArrayList<Cat> cats)
+//          — возвращает список котов кроме первого и последнего.
+//      TODO: 07.10.2022 Cat findFirstNonAngryCat (ArrayList<Cat> cats)
+//          — возвращает первого не-сердитого кота в списке.
+//      TODO: 07.10.2022 int getCommonWeight(ArrayList<Cat> cats, boolean onlyAngry)
+//          — возвращает суммарный вес всех котов
+//          (если параметр onlyAngry равен true, то только сердитых котов).
+//      TODO: 07.10.2022 Map<String, List<Cat>> groupCatsByFirstLetter (ArrayList<Cat> cats)
+//          — возвращает список котов сгруппировав их по первой букве имени
+//          и отсортировав группировку по возрастанию.
 
 
 public class App
---
+++ src/main/java/org/example/CatStatistics.java
@@ -0,0 +1,20 @@
+package org.example;
+
+// TODO: 07.10.2022 Описать класс CatStatistics со статическими методами:
+//      ArrayList<Cat> sortByNameAscending(ArrayList<Cat> cats) — возвращает список котов, отсортировав по имени по возрастанию.
+//      ArrayList<Cat> sortByWeightDescending(ArrayList<Cat> cats) — возвращает список котов, отсортировав по убыванию веса.
+//      ArrayList<Cat> removeFirstAndLast (ArrayList<Cat> cats) — возвращает список котов кроме первого и последнего.
+//      Cat findFirstNonAngryCat (ArrayList<Cat> cats) — возвращает первого не-сердитого кота в списке.
+//      int getCommonWeight(ArrayList<Cat> cats, boolean onlyAngry) — возвращает суммарный вес всех котов (если параметр onlyAngry равен true, то только сердитых котов).
+//      Map<String, List<Cat>> groupCatsByFirstLetter (ArrayList<Cat> cats) — возвращает список котов сгруппировав их по первой букве имени и отсортировав группировку по возрастанию.
+
+import java.util.ArrayList;
+
+public class CatStatistics {
+
+    ArrayList<Cat> sortByNameAscending(ArrayList<Cat> cats){
+        return cats;
+    }
+
+
+}
